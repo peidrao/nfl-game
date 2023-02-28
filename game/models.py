3e8cec174dec2e84f7ea6dd99af389d6cdcbfee5
Pedro Fonseca <contatopedrorn@gmail.com>
2023-02-28 00:08:58 -0300

    update models from game

--- game/models.py
+++ game/models.py
@@ -5,12 +5,15 @@ from team.models import Team
 
 class Season(models.Model):
     year = models.IntegerField()
-    profile = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True)
     is_active = models.BooleanField(default=True)
+
+
+class Week(models.Model):
+    name = models.CharField(max_length=150, null=True)
 
 
 class Match(models.Model):
     team1 = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True, related_name='match_team1')
     team2 = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True, related_name='match_team2')
     season = models.ForeignKey(Season, on_delete=models.SET_NULL, null=True)
-    profile = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True)
+    week = models.ForeignKey(Week, on_delete=models.SET_NULL, null=True)
