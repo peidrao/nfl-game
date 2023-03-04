d3e3bf97aabf77e25cbf56d3efa50d8a416c7de0
Pedro Fonseca <contatopedrorn@gmail.com>
2023-03-04 12:16:34 -0300

    refactor: update views

--- src/index/urls.py
+++ src/index/urls.py
@@ -1,24 +1,10 @@
 from django.urls import path
 
-from index.views import (
-    HomeView,
-    LoginView,
-    AccountView,
-    ConferencesView,
-    TeamsListView,
-    LogoutView,
-)
+from index.views import HomeView, LoginView, LogoutView
 
 
 urlpatterns = [
     path("", HomeView.as_view(), name="index"),
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
-    path("account/", AccountView.as_view(), name="account"),
-    path("conferences/", ConferencesView.as_view(), name="conferences"),
-    path(
-        "conferences/<int:conference_id>/teams/",
-        TeamsListView.as_view(),
-        name="teams_list",
-    ),
 ]
