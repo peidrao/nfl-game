8872facef06622eaff80aa880a2ffef5ea84c61b
Pedro Fonseca <contatopedrorn@gmail.com>
2023-03-10 23:12:21 -0300

    refactor: remove user app

--- src/user/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from user.views import AccountView, StartSeasonView
-
-
-urlpatterns = [
-    path("account/", AccountView.as_view(), name="account"),
-    path("season/start/", StartSeasonView.as_view(), name="start_season"),
-]
